dc14ca235109f84ccbcbb1a097f826dc09573e0e
mortenkjarulff <32191001+mortenkjarulff@users.noreply.github.com>
2017-10-12 16:19:42 +0200

    Update test4-template2.js

--- gsheet2page/test4-template2.js
+++ gsheet2page/test4-template2.js
@@ -26,7 +26,7 @@
         document.write('<title>' + sheetdata[idx_title[0]].gsx$value1.$t + '</title>');
         document.write('</head>');
         document.write('<body>');
-        document.write('<h1>template2</h1>');
+        document.write('<h1>hello template2</h1>');
         for (i=idx_post[0]; i<=idx_post[1]; i++) {
           document.write('post_value1[' + sheetdata[i].gsx$value1.$t + ']<br/>');
           document.write('post_value2[' + sheetdata[i].gsx$value2.$t + ']<br/>');
