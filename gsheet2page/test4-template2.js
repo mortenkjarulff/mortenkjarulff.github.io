67825ead6ebcb065e0abbd35fa066acd3854c2f5
mortenkjarulff <32191001+mortenkjarulff@users.noreply.github.com>
2017-10-12 16:14:24 +0200

    Update test4-template2.js

--- gsheet2page/test4-template2.js
+++ gsheet2page/test4-template2.js
@@ -1,64 +1,3 @@
-<!doctype html>
-<html>
-<head>
-<script>
-  var gsheet='1m5NxGi1nQF2OI77OdyJDu7mYjR1rG1KvGZHDgXmpZTw';
-</script>
-<script>
-  var sheetdata=null;
-  var idx_template=[-1,-1];
-
-  function savedata(json) {
-
-    sheetdata=json.feed.entry;
-
-    for (var i=0; i<sheetdata.length; i++) {
-      if (sheetdata[i].gsx$keyword.$t == 'template') {
-        idx_template[0]=i;
-        idx_template[1]=i;
-        break;
-      }
-    }
-
-    switch(sheetdata[idx_template[0]].gsx$value1.$t) {
-      case 'template1':
-        var idx_title=[-1,-1];
-        var idx_article=[-1,-1];
-        var i=0;
-        while (i<sheetdata.length) {
-          if (sheetdata[i].gsx$keyword.$t == 'title') {
-            idx_title[0]=i;
-            i++;
-            while (i<sheetdata.length && sheetdata[i].gsx$keyword.$t == 'title') {
-              i++;
-            }
-            idx_title[1]=i-1;
-            continue;
-          }
-          if (sheetdata[i].gsx$keyword.$t == 'article') {
-            idx_article[0]=i;
-            i++;
-            while (i<sheetdata.length && sheetdata[i].gsx$keyword.$t == 'article') {
-              i++;
-            }
-            idx_article[1]=i-1;
-            continue;
-          }
-          i++;
-
-        }
-        document.write('<title>' + sheetdata[idx_title[0]].gsx$value1.$t + '</title>');
-        document.write('</head>');
-        document.write('<body>');
-        document.write('<h1>template1</h1>');
-        for (i=idx_article[0]; i<=idx_article[1]; i++) {
-          document.write('article_value1[' + sheetdata[i].gsx$value1.$t + ']<br/>');
-          document.write('article_value2[' + sheetdata[i].gsx$value2.$t + ']<br/>');
-        }
-        document.write('</body>');
-        document.write('</html>');
-        break;
-      case 'template2':
         var idx_title=[-1,-1];
         var idx_post=[-1,-1];
         var i=0;
@@ -94,13 +33,3 @@
         }
         document.write('</body>');
         document.write('</html>');
-        break;
-      default:
-        break;
-    }
-
-  }
-</script>
-<script>
-  document.write('<script src="https://spreadsheets.google.com/feeds/list/' + gsheet + '/1/public/values?alt=json-in-script&callback=savedata"></'+'script>');
-</script>
