4f0d5f4455238b2b15b4213f7e97bbdb3c956acb
mortenkjarulff <32191001+mortenkjarulff@users.noreply.github.com>
2017-10-12 16:13:23 +0200

    Update test4-template1.js

--- gsheet2page/test4-template1.js
+++ gsheet2page/test4-template1.js
@@ -1,1 +1,35 @@
+        var idx_title=[-1,-1];
+        var idx_article=[-1,-1];
+        var i=0;
+        while (i<sheetdata.length) {
+          if (sheetdata[i].gsx$keyword.$t == 'title') {
+            idx_title[0]=i;
+            i++;
+            while (i<sheetdata.length && sheetdata[i].gsx$keyword.$t == 'title') {
+              i++;
+            }
+            idx_title[1]=i-1;
+            continue;
+          }
+          if (sheetdata[i].gsx$keyword.$t == 'article') {
+            idx_article[0]=i;
+            i++;
+            while (i<sheetdata.length && sheetdata[i].gsx$keyword.$t == 'article') {
+              i++;
+            }
+            idx_article[1]=i-1;
+            continue;
+          }
+          i++;
 
+        }
+        document.write('<title>' + sheetdata[idx_title[0]].gsx$value1.$t + '</title>');
+        document.write('</head>');
+        document.write('<body>');
+        document.write('<h1>template1</h1>');
+        for (i=idx_article[0]; i<=idx_article[1]; i++) {
+          document.write('article_value1[' + sheetdata[i].gsx$value1.$t + ']<br/>');
+          document.write('article_value2[' + sheetdata[i].gsx$value2.$t + ']<br/>');
+        }
+        document.write('</body>');
+        document.write('</html>');
